3d351bd2aa9532e72723cb03d41fa0e7206a313d
Denis Abrantes <denis.abrantes@hpe.com>
2024-02-08 14:35:03 -0600

    first commit

---
+++ home/views.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import json
+import requests
+sys.path.append("./experiment")
+
+import glob
+import torch
+import transformers 
+from transformers import AutoTokenizer
+from chat_format import ASSISTANT_PROMPT, EOS_TOKEN, get_chat_format
+from dataset_utils import load_or_create_dataset
+from finetune import get_model_and_tokenizer
+
+# Import MLDE packages
+from determined.experimental import client as det
+from determined import pytorch
+from flask import render_template, session, request, redirect, Response
+
+# Google Cloud packages
+import google.oauth2.id_token
+import google.auth.transport.requests
+
+from . import home_bp
+
+pipeline, tokenizer = None, None
+
+
+@home_bp.route('/')
+def index():
+    print("--> Loading Home Page")
+    return render_template('/home.html')
+
+
+@home_bp.route('/execute_query', methods=['POST'])
+def execute_query():
+    query = request.form.get('query')
+    google_auth_request = google.auth.transport.requests.Request()
+    cloud_functions_url = 'https://us-central1-dai-dev-554.cloudfunctions.net/postgres_api'
+
+    id_token = google.oauth2.id_token.fetch_id_token(google_auth_request, cloud_functions_url)
+
+    headers = {'Authorization': f'Bearer {id_token}', 'Content-Type': 'application/json'}
+    payload = {"query": query}
+
+    response = requests.request("post", cloud_functions_url, json=payload, headers=headers)
+
+    response_msg = {'message': response.text }
+    return Response(str(response_msg), status=200, mimetype='application/json')
+
+
+def load_model(model):
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        torch_dtype=torch.float16,
+        device_map="auto"
+    )
+
+    return tokenizer, pipeline
+
+
+
+@home_bp.route('/connect', methods=['POST'])
+def connect():
+    global pipeline
+    global tokenizer
+    
+    model_name = ""
+
+    try:
+        tokenizer, pipeline = load_model(model_name)
+        response_msg = {'message': 'Connection Successful' }
+        return Response(str(response_msg), status=200, mimetype='application/json')        
+    except Exception as ex:
+        response_msg = {'message': f"FAILED TO LOAD MODEL: {ex}" }
+        return Response(str(response_msg), status=500, mimetype='application/json')
+
