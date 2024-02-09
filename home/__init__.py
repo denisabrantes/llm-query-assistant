b0397c605d357b2c4c909a73a76db976cea2493e
Denis Abrantes <denis.abrantes@hpe.com>
2024-02-09 14:46:30 -0600

    Update __init__.py

--- home/__init__.py
+++ home/__init__.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+import sys
+sys.path.append("./experiment")
 
 home_bp = Blueprint('home_bp', __name__, template_folder='templates', static_folder='../static', static_url_path='assets', url_prefix='/')
 
