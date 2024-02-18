0db6b630f8586b2274f8d59c751d45ad9962348d
Denis Abrantes <denis.abrantes@hpe.com>
2024-02-18 17:11:20 -0600

    added caching

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import sys
+from flask import Flask
+from cache_common import cache
+from pathlib import Path
+
 sys.path.append("./home/experiment")
-from flask import Flask, Blueprint, render_template, jsonify,  request
+
 from home import home_bp
 
-
 app = Flask(__name__)
+cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': Path('/tmp')})
+
 app.register_blueprint(home_bp)
 app.secret_key = "secretkey123"
 
--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 import os
 import sys
 import json
-import requests
-
 import glob
 import torch
-import transformers 
+import requests
+import transformers
+from cache_common import cache
+from determined import pytorch
 from transformers import AutoTokenizer
+from determined.experimental import client as det
+from flask import render_template, request, Response
 
 sys.path.append("./experiment")
 
@@ -16,11 +19,6 @@ from chat_format import get_chat_format as get_chat_format
 from dataset_utils import load_or_create_dataset as load_or_create_dataset
 from finetune import get_model_and_tokenizer as get_model_and_tokenizer
 
-# Import MLDE packages
-from determined.experimental import client as det
-from determined import pytorch
-from flask import render_template, session, request, redirect, Response
-
 # Google Cloud packages
 import google.oauth2.id_token
 import google.auth.transport.requests
@@ -29,114 +27,106 @@ from . import home_bp
 
 ip_address = "34.66.149.119"
 
-class FlaskBackend(object):
-    def __init__(self, app, **configs):
-        self.app = app
-        self.configs(**configs)
-        self.pipeline = None
-        self.tokenizer = None
-        self.models = None
-        self.datasets = None
 
-    @home_bp.route('/')
-    def index(self):
-        #print("--> Loading Home Page", flush=True)
-        if self.pipeline is None:
-            self.model_status = "not_loaded"
-        else:
-            model_status = "loaded"
-        return render_template('/home.html', hostname = ip_address, port = 8080, model_status = model_status)
+def get_model_list():
+    try:
+        f = open('/app/client/home/models.json')
+        return json.load(f)
+    except Exception as ex:
+        print(f"--> FAILED TO LIST MODELS: {ex}")
 
+def get_model_type(model_name):
+    model_list = get_model_list()
+    for model in model_list:
+        if model['name'] == model_name:
+            model_type = model['type']    
+    return model_type
 
-    @home_bp.route('/connect', methods=['POST'])
-    def connect(self):
-        if self.pipeline is None:
-            model_status = "not_loaded"
-        else:
-            model_status = "loaded"
-        response_msg = {'message': "Connected", "model_status" : model_status }
-        return Response(str(response_msg), status=200, mimetype='application/json')
+@home_bp.route('/')
+def index():
+    print("--> Loading Home Page", flush=True)
+    return render_template('/home.html', hostname = ip_address, port = 8080)
 
+@home_bp.route('/connect', methods=['POST'])
+def connect():
+    response_msg = {'message': "Connected"}
+    return Response(str(response_msg), status=200, mimetype='application/json')
 
-    @home_bp.route('/execute_query', methods=['POST'])
-    def execute_query(self):
-        query = request.form.get('query')
-        google_auth_request = google.auth.transport.requests.Request()
-        cloud_functions_url = 'https://us-central1-dai-dev-554.cloudfunctions.net/postgres_api'
+@home_bp.route('/execute_query', methods=['POST'])
+def execute_query():
+    query = request.form.get('query')
+    google_auth_request = google.auth.transport.requests.Request()
+    cloud_functions_url = 'https://us-central1-dai-dev-554.cloudfunctions.net/postgres_api'
 
-        id_token = google.oauth2.id_token.fetch_id_token(google_auth_request, cloud_functions_url)
+    id_token = google.oauth2.id_token.fetch_id_token(google_auth_request, cloud_functions_url)
 
-        headers = {'Authorization': f'Bearer {id_token}', 'Content-Type': 'application/json'}
-        payload = {"query": query}
+    headers = {'Authorization': f'Bearer {id_token}', 'Content-Type': 'application/json'}
+    payload = {"query": query}
 
-        response = requests.request("post", cloud_functions_url, json=payload, headers=headers)
+    response = requests.request("post", cloud_functions_url, json=payload, headers=headers)
 
-        response_msg = {'message': response.text }
-        return Response(str(response_msg), status=200, mimetype='application/json')
+    response_msg = {'message': response.text }
+    return Response(str(response_msg), status=200, mimetype='application/json')
 
+@home_bp.route('/load_model', methods=['POST'])
+def load_model():
+    model_name = request.values.get('model_id')
+    model_type = get_model_type(model_name)
 
-    @home_bp.route('/load_model', methods=['POST'])
-    def load_model(self):
-        isLocalModel = False
-
-        model_name = request.values.get('model_id')
-        if "local" in model_name:
-            model_name = "TinyLlama/TinyLlama-1.1B-Chat-v0.4" 
-            isLocalModel = True
-
-        if isLocalModel:
-            try:
-                self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-                self.pipeline = transformers.pipeline(
-                    "text-generation",
-                    model=model_name,
-                    torch_dtype=torch.float16,
-                    device_map="auto"
-                )
-                response_msg = {'message': f"Model Loaded Successfully: {model_name}", "model_status" : "loaded" }
-                return Response(str(response_msg), status=200, mimetype='application/json')
-            except Exception as ex:
-                response_msg = {'message': f"FAILED TO LOAD MODEL: {ex}", "model_status" : "not_loaded" }
-                return Response(str(response_msg), status=500, mimetype='application/json')
-
-
-    @home_bp.route('/list_models', methods=['GET'])
-    def list_models(self):
-        if self.pipeline is None:
-            model_status = "not_loaded"
-        else:
-            model_status = "loaded"
+    if "hf" in model_type:
         try:
-            f = open('/app/client/home/models.json')
-            self.models = json.load(f)
-            response_msg = {'models': self.models, "model_status" : model_status }
+            tokenizer = AutoTokenizer.from_pretrained(model_name)
+            pipeline = transformers.pipeline(
+                "text-generation",
+                model=model_name,
+                torch_dtype=torch.float16,
+                device_map="auto"
+            )
+
+            cache.set("tokenizer", tokenizer)
+            cache.set("pipeline", pipeline)
+            cache.set("active_model", model_name)
+
+            response_msg = {'message': f"Model Loaded Successfully: {model_name}"}
             return Response(str(response_msg), status=200, mimetype='application/json')
         except Exception as ex:
-            response_msg = {'message': f"FAILED TO LOAD MODEL LIST: {ex}", "model_status" : model_status }
+            response_msg = {'message': f"FAILED TO LOAD MODEL: {ex}"}
             return Response(str(response_msg), status=500, mimetype='application/json')
 
+@home_bp.route('/list_models', methods=['GET'])
+def list_models():
+    try:
+        models = get_model_list()
+        response_msg = {'models': models}
+        return Response(str(response_msg), status=200, mimetype='application/json')
+    except Exception as ex:
+        response_msg = {'message': f"FAILED TO LOAD MODEL LIST: {ex}"}
+        return Response(str(response_msg), status=500, mimetype='application/json')
 
-    @home_bp.route('/list_datasets', methods=['GET'])
-    def list_datasets(self):
-        try:
-            f = open('/app/client/home/datasets.json')
-            self.datasets = json.load(f)
-            return Response(str(self.datasets), status=200, mimetype='application/json')
-        except Exception as ex:
-            response_msg = {'message': f"FAILED TO LOAD MODEL LIST: {ex}" }
-            return Response(str(response_msg), status=500, mimetype='application/json')
+@home_bp.route('/list_datasets', methods=['GET'])
+def list_datasets():
+    try:
+        f = open('/app/client/home/datasets.json')
+        datasets = json.load(f)
+        return Response(str(datasets), status=200, mimetype='application/json')
+    except Exception as ex:
+        response_msg = {'message': f"FAILED TO LOAD MODEL LIST: {ex}" }
+        return Response(str(response_msg), status=500, mimetype='application/json')
 
-
-    @home_bp.route('/question', methods=['POST'])
-    def answer_question(self):
+@home_bp.route('/question', methods=['POST'])
+def answer_question():
             try:
-                eos_token_id = self.tokenizer.get_vocab()[EOS_TOKEN]
-                prompt = request.values.get('prompt')
-                model_id = request.values.get('model')
+                tokenizer = cache.get("tokenizer")
+                pipeline = cache.get("pipeline")
 
-                if "local" in model_id:
+                eos_token_id = tokenizer.get_vocab()[EOS_TOKEN]
+                prompt = request.values.get('prompt')
+                model_name = request.values.get('model')
+                model_type = get_model_type(model_name)
+
+                if "hf" in model_type:
                     try:
-                        sequences = self.pipeline(
+                        sequences = pipeline(
                             prompt,
                             do_sample=True,
                             top_k=50,
